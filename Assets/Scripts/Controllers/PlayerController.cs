a6154a5d2236e3d65b972d7a95c6adec2b990c23
Rodrigo Medina Amador <rmedinaamador@gmail.com>
2021-01-16 16:47:57 +0100

    Change namespace for controllers.

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using Model;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private float _inputHorizontalSpeed;
+        private float _inputVerticalSpeed;
+        private Rigidbody _rigidbody;
+
+        private Character _character;
+
+        public Character Character
+        {
+            set => _character = value;
+        }
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            _inputHorizontalSpeed = Input.GetAxis("Horizontal") * _character.speed;
+            _inputVerticalSpeed = Input.GetAxis("Vertical") * _character.speed;
+        
+            _rigidbody.velocity = new Vector2(_inputHorizontalSpeed, _inputVerticalSpeed);
+        }
+    }
+}
